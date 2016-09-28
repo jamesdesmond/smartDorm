a0f5db71243244332a7350408b7b68819efee9a0
James R. Desmond <desmondj@wit.edu>
2016-09-27 20:44:57 -0400

    Testing out a menu system demo

--- src/Runner.java
+++ src/Runner.java
@@ -10,14 +10,16 @@ import java.io.IOException;
 public class Runner {
     public static void main(String Args[]) throws IOException {
         final ILCD lcd = new RealLCD();
+
         lcd.setBacklight(Color.RED);
         lcd.setText("This is the \n start screen");
-        String address = Util.getLocalAddress().toString().substring(1);
+        showMenu(lcd,1);
         ButtonPressedObserver observer = new ButtonPressedObserver(lcd);
         observer.addButtonListener(new ButtonListener() {
             @Override
             public void onButtonPressed(Button button) {
                 try {
+                    int currentMenu = 0;
                     switch (button) {
                         case RIGHT:
                             lcd.clear();
@@ -28,12 +30,10 @@ public class Runner {
                             lcd.setText("LEFT");
                             break;
                         case DOWN:
-                            lcd.clear();
-                            lcd.setText("DOWN");
+                            showMenu(lcd,currentMenu++);
                             break;
                         case UP:
-                            lcd.clear();
-                            lcd.setText("UP");
+                            showMenu(lcd,currentMenu--);
                             break;
                         case SELECT:
                             showIP.showIP(lcd);
@@ -49,5 +49,32 @@ public class Runner {
             }
         });
         while(true){}
+    }
+    private static void showMenu(ILCD ilcd, int i) throws IOException {
+        switch (i) {
+            case 0:
+                showMenu(ilcd,4);
+                System.out.println("Shifted to case 4.   case:0");
+                break;
+            case 1:
+                ilcd.setText("Case 1:\nThe Weather");
+                System.out.println("case:1");
+                break;
+            case 2:
+                ilcd.setText("Case 2:\nText Carter");
+                System.out.println("case:2");
+                break;
+            case 3:
+                ilcd.setText("Case 3:\nText James");
+                System.out.println("case:3");
+                break;
+            case 4:
+                showIP.showIP(ilcd);
+                System.out.println("case:4");
+                break;
+            case 5:
+                showMenu(ilcd,1);
+                System.out.println("Shifted to case 1. case:5");
+        }
     }
 }
