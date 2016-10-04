8c927636ac9520c2fc993e1e43bc6823381c707b
James R. Desmond <desmondj@wit.edu>
2016-10-04 00:31:49 -0400

    making sure the API key isn't committed

--- src/showWeather.java
+++ src/showWeather.java
@@ -1,8 +1,12 @@
+import com.github.dvdme.ForecastIOLib.ForecastIO;
+import com.oracle.tools.packager.IOUtils;
 import se.hirt.pi.adafruitlcd.Button;
 import se.hirt.pi.adafruitlcd.ButtonListener;
 import se.hirt.pi.adafruitlcd.ButtonPressedObserver;
 import se.hirt.pi.adafruitlcd.ILCD;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -12,7 +16,12 @@ public class showWeather {
     public static void showWeather (ILCD ilcd) throws IOException {
         ilcd.clear();
         ilcd.setText("Hey its the weather");
-
+        FileInputStream inputStream = new FileInputStream("api.txt");
+        String api = inputStream.toString();
+        ForecastIO fio = new ForecastIO(api);
+        fio.setUnits(ForecastIO.UNITS_US);             //sets the units as SI - optional
+        fio.setExcludeURL("hourly,minutely");             //excluded the minutely and hourly reports from the reply
+        fio.getForecast("42.3605","-71.0596");
 
         ButtonPressedObserver observer = new ButtonPressedObserver(ilcd);
         observer.addButtonListener(new ButtonListener() {
