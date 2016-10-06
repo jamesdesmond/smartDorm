15cd5ae1851d7a3f2fefabc24d71d37aaf926f65
James R. Desmond <desmondj@wit.edu>
2016-10-06 17:02:07 -0400

    Narrowing down spikes problem

--- src/showWeather.java
+++ src/showWeather.java
@@ -26,7 +26,6 @@ public class showWeather {
         ilcd.clear();
         ilcd.setText("Hey its the weather");
         String api = new String(Files.readAllBytes(Paths.get("api.txt")));
-        api = api.substring(0,32);
         System.out.println("api: " + api + "length: " + api.length());
         //DEBUG
         for (int i = 0; i < api.length(); i++) {
