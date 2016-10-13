c05c07a453d6c187e2976ba6e1761f685c5692ee
James R. Desmond <desmondj@wit.edu>
2016-10-12 21:03:44 -0400

    testing a multiple arg command

--- src/sendText.java
+++ src/sendText.java
@@ -15,8 +15,8 @@ public class sendText {
     private static void sendText(String address, String message) {
         System.out.println("sendText()0");
         //String command = "bash", "-c", \"mutt -F /root/.muttrc -s \"SmartDorm\" 8608332915@vtext.com <<< \\\"I need the room for a little bit\\\"\"";
-        //String[] command = {"bash","-c","\"mutt -F /root/.muttrc -s \"SmartDorm\" 8608332915@vtext.com <<< \\\"I need the room for a little bit\\\"\""};
-        String[] command = {"touch","/home/pi/test.txt"};
+        String[] command = {"mutt" ,  "-F" ,  "/root/.muttrc" ,  "-s" ,  "\"SmartDorm\"", "8608332915@vtext.com" , "<<<" ,  "\"I need the room for a little bit\""};
+        //String[] command = {"touch","/home/pi/test.txt"};
         System.out.println(Arrays.toString(command));
         /*int i = Runtime.getRuntime().exec(command).waitFor();
         try {
