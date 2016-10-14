97c58f4a935467f1cdef8f52296faa1f8a6622d6
James R. Desmond <desmondj@wit.edu>
2016-10-13 23:47:03 -0400

    trying to fix my mistake of messing with the while true

--- src/Runner.java
+++ src/Runner.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class Runner {
     private  static final LCDApps[] APPS = new LCDApps[]{
-            new showWeather(),new sendText(Enums.People.CARTER),new sendText(Enums.People.JAMES),new showIP(),//new sleepDisplay()
+            new showWeather(),new sendText(Enums.People.CARTER),new sendText(Enums.People.JAMES),new showIP(),new sleepDisplay()
     };
     public static void main(ILCD ilcd) throws IOException {
         int[] currentMenu = {0};
@@ -33,6 +33,8 @@ public class Runner {
                             System.out.println("Down: " + currentMenu[0]);
                             currentMenu[0] = currentMenu[0] + 1;
                             System.out.println("current menu is now: " + currentMenu[0]);
+                            //System.out.println(currentMenu + "DOWN");
+                            //currentMenu = currentMenu == MAX_VALUE?0:currentMenu; //JANKY WAY OF HANDLING BOUNDS
                             if (currentMenu[0] > APPS.length -1) {
                                 currentMenu[0] = 0;
                                 System.out.println("currentMenu was equal to 4, now equal to 0");
@@ -46,6 +48,8 @@ public class Runner {
                             System.out.println("Up: " + currentMenu[0]);
                             currentMenu[0]--;
                             System.out.println("current menu is now: " + currentMenu[0]);
+                            //System.out.println(currentMenu + "UP");
+                            //currentMenu = currentMenu == 0?MAX_VALUE:currentMenu; //JANKY WAY OF HANDLING BOUNDS
                             if (currentMenu[0] < 0) {
                                 currentMenu[0] = APPS.length -1;
                                 System.out.println("currentMenu was equal to -1, now equal to 3");
@@ -67,7 +71,7 @@ public class Runner {
                 }
             }
         });
-        while (true) {} //TODO: stop doing this
+        while(true) {}
     }
     public static void main(String Args[]) throws IOException {
         final ILCD lcd = new RealLCD();
