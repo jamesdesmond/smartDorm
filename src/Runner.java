974eac81fe6f5b15c76bf15d317b5b6c338a1b1c
James R. Desmond <desmondj@wit.edu>
2016-10-13 23:57:27 -0400

    WORKING COMMIT

--- src/Runner.java
+++ src/Runner.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class Runner {
     private  static final LCDApps[] APPS = new LCDApps[]{
-            new showWeather(),new sendText(Enums.People.CARTER),new sendText(Enums.People.JAMES),new showIP()
+            new showWeather(),new sendText(Enums.People.CARTER),new sendText(Enums.People.JAMES),//new showIP(),new sleepDisplay()
     };
     public static void main(ILCD ilcd) throws IOException {
         int[] currentMenu = {0};
@@ -75,6 +75,6 @@ public class Runner {
     }
     public static void main(String Args[]) throws IOException {
         final ILCD lcd = new RealLCD();
-        main(lcd);
+
     }
 }
